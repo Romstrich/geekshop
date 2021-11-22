46fd2178f7f47c34313d00994ee6e8719a1fb0c2
Roman <roman.motrich@yandex.ru>
2021-11-22 16:27:53 +0300

    ДЗ 2 Готово

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context={
+        'title':'Geekshop',
+
+    }
+    return render(request, 'mainapp/index.html',context)
+
 
 def products(request):
-    return render(request, 'products.html')
+    context = {
+        'title': 'Geetshop Каталог',
+
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+# def test(request):
+#     context = {
+#         'title': 'geekshop_test',
+#         'main_header':'Проверка контекста',
+#         'welcome':'Приветствую, ',
+#         'user':'Ромыч',
+#         'items':[
+#             {'name':'Худи черного цвета с монограммами adidas Originals','price':6900},
+#             {'name': 'Синяя куртка The North Face', 'price': 23725},
+#             {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
+#             {'name': 'Черный рюкзак Nike Heritage', 'price': 2340},
+#             {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 13590},
+#             {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2890}
+#     ],
+#     }
+    return render(request, 'test.html', context)
